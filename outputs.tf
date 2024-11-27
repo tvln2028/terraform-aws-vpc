bb03e61bce9b50f856900a0e34e3edf0477ab11f
tvln2028 <lakshminarayana4560@gmail.com>
2024-11-27 17:02:59 -0500

    sg

--- outputs.tf
+++ outputs.tf
@@ -23,6 +23,10 @@ output "database_subnet_group_id" {
     value = aws_db_subnet_group.default.id
 }
 
+output "database_subnet_group_name" {
+    value = aws_db_subnet_group.default.name
+}
+
 output "igw_id" {
     value = aws_internet_gateway.gw.id
 }
